ccd47d0c3ab5c5b264ffb4fa27bbb07ccf9854e0
tarabogart13 <33662098+tarabogart13@users.noreply.github.com>
2017-11-17 08:36:33 -0500

    Update Enums.java

--- src/main/java/de/logicline/splash/utils/Enums.java
+++ src/main/java/de/logicline/splash/utils/Enums.java
@@ -1,13 +1,11 @@
 package de.logicline.splash.utils;
 
 /** 
- * 
  * UserService is part of the model in the MVC pattern
  * It is handling the business logic, is between controller and Persistence layer
  * 
  * Enums for user roles of the WebApp User
  */
 public class Enums {
-	
-	  public static enum UserRole {ROLE_ADMIN, ROLE_CSC_AGENT, ROLE_CUSTOMER};
+	public static enum UserRole {ROLE_ADMIN, ROLE_CSC_AGENT, ROLE_CUSTOMER};
 }
