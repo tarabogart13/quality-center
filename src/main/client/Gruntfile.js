7976fa39566b7a6a1b42f15dc3bce8a57cbe5352
tarabogart13 <33662098+tarabogart13@users.noreply.github.com>
2017-11-20 08:26:24 -0500

    Update Gruntfile.js

--- src/main/client/Gruntfile.js
+++ src/main/client/Gruntfile.js
@@ -13,10 +13,7 @@ module.exports = function(grunt) {
 		'bower_components/angular-local-storage/dist/angular-local-storage.min.js',
 		'bower_components/ng-file-upload/angular-file-upload.min.js',
 		'bower_components/ng-file-upload/angular-file-upload-shim.min.js',
-		'bower_components/angular-recaptcha/release/angular-recaptcha.min.js',
-		'bower_components/angular-material/angular-material.min.js',
-		'bower_components/angular-material/angular-material.css',
-		'bower_components/angular-material-data-table/dist/md-data-table.min.js'
+		'bower_components/angular-recaptcha/release/angular-recaptcha.min.js'
 	];
 
 	grunt.initConfig({
